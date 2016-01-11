2eb9406a05445a78f16e2c6de448809ce3f3e2b2
xyder <xydr2311@gmail.com>
2016-01-11 17:27:34 +0200

    Added 'add' and 'update' endpoints to the rest api.

--- web_api/response_builders.py
+++ web_api/response_builders.py
@@ -23,37 +23,61 @@ def build_link(**kwargs):
 
 def build_add_link():
     return build_link(
-        rel='add',
-        href=config.api_url_start,
-        method=Methods.POST,
-        args=[]
+            rel='add',
+            href=config.api_url_start,
+            method=Methods.POST,
+            args=[]
     )
 
 
 def build_self_link(item_id=None):
     url = config.api_url_start + ('' if not item_id else '/%s' % item_id)
     return build_link(
-        rel='self',
-        href=url,
-        method=Methods.GET
+            rel='self',
+            href=url,
+            method=Methods.GET
     )
 
 
 def build_delete_link(item_id):
     return build_link(
-        rel='delete',
-        href='%s/%s' % (config.api_url_start, item_id),
-        method=Methods.DELETE,
+            rel='delete',
+            href='%s/%s' % (config.api_url_start, item_id),
+            method=Methods.DELETE,
     )
 
 
 def build_update_link(item_id):
     return build_link(
-        rel='update',
-        href='%s/%s' % (config.api_url_start, item_id),
-        method=Methods.POST,
-        args=[]
+            rel='update',
+            href='%s/%s' % (config.api_url_start, item_id),
+            method=Methods.POST,
+            args=[]
     )
+
+
+def update_or_init(values):
+    return {
+        'title': values.get('title', ''),
+        'body': values.get('body', ''),
+        'created': values.get('created', 0),
+        'due': values.get('due', 0),
+        'state': values.get('state', 'open')
+    }
+
+
+def join_kv(dict1, dict2, key, default=None):
+    return dict1.get(key) if key in dict1 else dict2.get(key, default)
+
+
+def join_dicts(old_item, updated_values):
+    return {
+        'title': join_kv(updated_values, old_item, 'title', ''),
+        'body': join_kv(updated_values, old_item, 'body', ''),
+        'created': join_kv(updated_values, old_item, 'created', 0),
+        'due': join_kv(updated_values, old_item, 'due', 0),
+        'state': join_kv(updated_values, old_item, 'state', 'open')
+    }
 
 
 def convert_item_list(items_cursor):
@@ -100,3 +124,17 @@ def build_response(**kwargs):
 
     }
     return jsonify(resp), resp['status_code']
+
+
+def get_404():
+    return build_response(
+            status='Not found.',
+            status_code=404
+    )
+
+
+def get_400():
+    return build_response(
+            status='Bad request.',
+            status_code=400
+    )
--- web_api/routes.py
+++ web_api/routes.py
@@ -1,28 +1,65 @@
 from pymongo import MongoClient
 from bson import ObjectId
 from bson.errors import InvalidId
-from flask import Flask
+from flask import Flask, request
+from werkzeug.exceptions import HTTPException
 
 import config
-from web_api.response_builders import build_response
+from web_api.response_builders import build_response, get_404, get_400, update_or_init, join_dicts
 
 app = Flask(__name__)
 
 
-@app.route('/api/v%s/tasks' % config.api_version, methods=['GET'])
-@app.route('/api/v%s/tasks/<item_id>' % config.api_version, methods=['GET'])
+@app.route(config.api_url_start, methods=['GET'])
+@app.route('%s/<item_id>' % config.api_url_start, methods=['GET'])
 def get_tasks(item_id=None):
     client = MongoClient()
     db = client.tasks_db
     query_filter = {}
+
     if item_id is not None:
         try:
             query_filter['_id'] = ObjectId(item_id)
         except InvalidId as _:
-            return build_response(
-                status='Not Found.',
-                status_code=404
-            )
+            return get_400()
+        cursor = db.tasks.find(query_filter)
+        if cursor.count() == 0:
+            return get_404()
+    else:
+        cursor = db.tasks.find()
+
     return build_response(
-        items_cursor=db.tasks.find(query_filter)
+        items_cursor=cursor
     )
+
+
+@app.route(config.api_url_start, methods=['POST'])
+@app.route('%s/<item_id>' % config.api_url_start, methods=['POST'])
+def add_edit_item(item_id=None):
+    client = MongoClient()
+    db = client.tasks_db
+    query_filter = {}
+
+    try:
+        new_data = request.json
+    except HTTPException as e:
+        if e.code == 400:
+            return get_400()
+        else:
+            return build_response(
+                status=e.description,
+                status_code=e.code
+            )
+
+    if item_id is not None:
+        try:
+            query_filter['_id'] = ObjectId(item_id)
+            old_item = db.tasks.find_one(query_filter)
+            db.tasks.replace_one(query_filter, join_dicts(old_item, new_data))
+            return build_response(items_cursor=db.tasks.find(query_filter))
+        except InvalidId as _:
+            return get_404()
+    else:
+        res = db.tasks.insert_one(update_or_init(new_data))
+        query_filter['_id'] = res.inserted_id
+        return build_response(items_cursor=db.tasks.find(query_filter))
